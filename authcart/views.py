94c1f801f95c9ccc93e18373737173abe19bd661
ngevaotamba <ngevaotamba9@gmail.com>
2023-06-06 23:15:21 -0700

    My first comment

---
+++ authcart/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect,HttpResponse
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def signup(request):
+    if request.method=="POST":
+        email-request.POST['email']
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        if password!=confirm_password:
+            return HttpResponse("password incorrect")
+        # return render(request,'auth/signup.html')
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse("email already exist")
+            # return render(request,'auth/signup.html')
+        except Exception as identtifier:
+            pass
+        user= User.objects.create_user(email,email,password)
+        user.save()
+        return HttpResponse("User created",email)
+    return render(request,"signup.html")
+
+def handlelogin(request):
+    return render(request, "login.html")
+
+
+def handlelogout(request):
+    return redirect("/auth/login")
